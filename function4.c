9d2e827e8227bb533fcfd737f48d1a489b38d9d5
eylens <eymasac@gmail.com>
2021-07-29 01:32:58 +0700

    new func pr_zero

--- function4.c
+++ function4.c
@@ -2,17 +2,18 @@
 /**
  * pr_ui - Unsigned decimal integer
  *
- * @arg: argyments
+ * @arg: arguments
  * Return: 0
  */
 int pr_ui(va_list __attribute__((unused))arg) /*if (*p == 'u')*/
 {
 	int i;
-	unsigned int long ui;
 	char *s;
 	char buffer[1024];
 	char buffer_tmp[20];
 	int size_write = 0;
+
+	unsigned int long ui;
 	ui = (unsigned int)INT_MAX + 1024;
 
 	if (ui <= (unsigned int) INT_MAX)
@@ -20,17 +21,28 @@ int pr_ui(va_list __attribute__((unused))arg) /*if (*p == 'u')*/
 		i = (int) ui;
 		s = _itoa(i, buffer, 10);
 		size_write = strlen(s);
-		}
-		else
-		{
-			i = (int) (ui / 1000000000);
-			s = _itoa(i, buffer, 10);
-			size_write = strlen(s);
-			i = (int) (ui - ((unsigned int) i) * 1000000000);
-			_itoa(i, buffer_tmp, 10);
-			strcat(buffer, buffer_tmp);
-			size_write = strlen(buffer);
-			}
-			write(2, s, size_write);
-			return (0);
+	}
+	else
+	{
+		i = (int) (ui / 1000000000);
+		s = _itoa(i, buffer, 10);
+		size_write = strlen(s);
+		i = (int) (ui - ((unsigned int) i) * 1000000000);
+		_itoa(i, buffer_tmp, 10);
+		strcat(buffer, buffer_tmp);
+		size_write = strlen(buffer);
+	}
+	write(1, s, size_write);
+	return (0);
+}
+/**
+ * pr_zero - When the users try _printf("%"), without indicator specifier
+ *
+ * @arg: arguments
+ * Return: Prints "Error"
+ */
+int pr_zero(va_list __attribute__((unused))arg) /* if (*p == '\0') */
+{
+	write(1, "Error\n", 6);
+	exit(-1);
 }
